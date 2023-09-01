a2a3bc31a5721bd89fd3294e9d1c1fc9de7e41ff
kireeti <rayaprolu.kireeti@gmail.com>
2023-09-01 10:09:31 -0400

    @Bean, @Configuration annotation implementation. Understanding Spring Context and AnnotationConfigApplicationContext.

---
+++ Example1/src/main/java/com/example/main/Example1.java
@@ -0,0 +1,21 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Vehicle vehicle=new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from outside spring context is: "+ vehicle.getName());
+        var context=new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh=context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+        String hello=context.getBean(String.class);
+        System.out.println("String value from spring context is :"+ hello);
+        Integer num=context.getBean(Integer.class);
+        System.out.println("Integer value from spring context is :"+ num);
+
+    }
+}
