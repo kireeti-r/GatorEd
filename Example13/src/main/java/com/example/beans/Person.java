31dd560c3d86bf3b37e87b70378ce86deae42c98
kireeti <rayaprolu.kireeti@gmail.com>
2023-09-02 04:56:17 -0400

    @Autowired on top of class constructor

---
+++ Example13/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,30 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    public Person(){
+        System.out.println("Person bean created by Spring");
+    }
+    private String name="Lucy";
+
+    private Vehicle vehicle;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    @Autowired
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
