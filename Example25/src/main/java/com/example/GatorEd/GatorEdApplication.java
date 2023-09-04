1e62d68766972ccae3502021be7e93f19573aee1
kireeti <rayaprolu.kireeti@gmail.com>
2023-09-04 14:54:21 -0400

    Simple Spring Boot Web Application

---
+++ Example25/src/main/java/com/example/GatorEd/GatorEdApplication.java
@@ -0,0 +1,13 @@
+package com.example.GatorEd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatorEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatorEdApplication.class, args);
+	}
+
+}
