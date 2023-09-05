fa790bb033bc98ed389d14c4456d59f043aa463d
kireeti <rayaprolu.kireeti@gmail.com>
2023-09-05 08:20:29 -0400

    Adding dynamic content to the webpage using Thymleaf and introduction to Model attributes

--- Example27/src/main/java/com/example/GatorEd/Controller/HomeController.java
+++ Example27/src/main/java/com/example/GatorEd/Controller/HomeController.java
@@ -2,11 +2,13 @@ package com.example.GatorEd.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
     @RequestMapping(value={"/home", "", "/"})
-    public String displayHomePage(){
+    public String displayHomePage(Model model){
+        model.addAttribute("username", "John Doe");
         return "home.html";
     }
 
