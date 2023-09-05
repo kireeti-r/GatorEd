663b77208e0baaf70ce3e8740f5a875c06e698d5
kireeti <rayaprolu.kireeti@gmail.com>
2023-09-05 16:20:13 -0400

    GatorEd HomePage

---
+++ Example28/src/main/java/com/example/GatorEd/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.GatorEd.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value={"/home", "", "/"})
+    public String displayHomePage(){
+
+        return "home.html";
+    }
+
+
+}
